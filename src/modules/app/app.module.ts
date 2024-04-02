9151eead185c84dcbf65f18798c1d1638f33a4cc
PixelWinner <alex.pixelwinner@gmail.com>
2024-04-02 19:18:03 +0300

    Vercel Fix

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,9 +2,11 @@ import { ConfigModule } from "@nestjs/config";
 import { Module } from "@nestjs/common";
 
 import { ChatGateway } from "../../gatewayes/chat/chat.gateway";
+import { AppController } from "./app.controller";
 
 @Module({
     imports: [ConfigModule.forRoot({ envFilePath: ".env" })],
+    controllers:[AppController],
     providers: [ChatGateway]
 })
 export class AppModule {
